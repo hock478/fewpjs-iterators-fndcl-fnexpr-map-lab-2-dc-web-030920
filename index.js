7ff8d0cb80e017ed98087014d704d84a59e72e13
Dawit Gizaw <gizawdawit123.com>
2020-04-20 09:33:40 -0400

    Done.

--- index.js
+++ index.js
@@ -12,5 +12,25 @@ const questions = [
 ];
 
 const capitalizeSentence = () => {
-  return questions
+
+  let cap = questions.map(function(question){
+    return question.charAt(0).toUpperCase() + question.slice(1)
+  });
+  return cap;
+  
+  // let cap = [
+  //   'What does the this keyword mean?',
+  //   'What is the Contutorialuctor OO pattern?',
+  //   'How can you implement a Blockchain Web API?',
+  //   'What is Test Driven Development Workflow?',
+  //   'What is NaN and how can we check for it?',
+  //   'What is the difference between stopPropagation and preventDefault?',
+  //   'Should you use immutable state and pure functions?',
+  //   'What is the difference between == and ===?',
+  //   'What is the difference between event capturing and bubbling?',
+  //   'What is JSONP?'
+  // ];
+  // return cap;
 }
+
+
